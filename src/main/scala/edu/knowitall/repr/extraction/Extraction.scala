b7ac85672d70db5e89524329abe76febdb9251b8
Rob Bart <rbart@cs.washington.edu>
2013-10-29 13:12:43 -0700

    Fix ugly filter vs filterNot bug.

--- src/main/scala/edu/knowitall/repr/extraction/Extraction.scala
+++ src/main/scala/edu/knowitall/repr/extraction/Extraction.scala
@@ -34,7 +34,7 @@ object Extraction {
 
   def fromSrlie(inst: SrlExtractionInstance, confidence: Double): Seq[Extraction] = {
     def tokenIndices(part: SrlExtraction.MultiPart) = part.intervals.flatten
-    inst.triplize(true).filter(_.extr.arg2s.isEmpty).flatMap { inst =>
+    inst.triplize(true).filterNot(_.extr.arg2s.isEmpty).flatMap { inst =>
       val rel = ExtractionPartImpl(inst.extr.rel.text, inst.extr.rel.intervals.flatten.min, inst.extr.rel.tokenIntervals.flatten)
       val arg1 = ExtractionPartImpl(inst.extr.arg1.text, inst.extr.arg1.interval.start, inst.extr.arg1.tokenInterval)
       inst.extr.arg2s.map { arg2 =>
