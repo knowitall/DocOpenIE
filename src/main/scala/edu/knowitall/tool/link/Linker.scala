b253c659461e13fc59c99b74a10a80d7b27ed80d
Rob Bart <rbart@cs.washington.edu>
2013-11-01 15:39:18 -0700

    Use Best Entity Mention in linker when possible.

--- src/main/scala/edu/knowitall/tool/link/Linker.scala
+++ src/main/scala/edu/knowitall/tool/link/Linker.scala
@@ -1,16 +1,20 @@
 package edu.knowitall.tool.link
 
+import edu.knowitall.collection.immutable.Interval
 import edu.knowitall.repr.sentence.Sentence
 import edu.knowitall.repr.sentence.Postagged
 import edu.knowitall.repr.sentence.Chunked
 import edu.knowitall.tool.sentence.OpenIEExtracted
 import edu.knowitall.tool.coref.CorefResolver
+import edu.knowitall.repr.bestentitymention.BestEntityMentionResolvedDocument
 import edu.knowitall.repr.link.Link
 import edu.knowitall.repr.link.FreeBaseLink
 import edu.knowitall.repr.coref.CorefResolved
 import edu.knowitall.repr.extraction.ExtractionPart
 import edu.knowitall.tool.coref.Mention
 import edu.knowitall.repr.coref.MentionCluster
+import edu.knowitall.tool.coref.Substitution
+import edu.knowitall.tool.coref.CoreferenceResolver
 import edu.knowitall.repr.link.LinkedDocument
 import edu.knowitall.repr.document.Document
 import edu.knowitall.repr.document.DocumentSentence
@@ -26,7 +30,6 @@ trait Linker {
 
   def link(doc: document): Seq[link]
 }
-
 
 trait OpenIELinked extends LinkedDocument {
   this: Document with Sentenced[Sentence with OpenIEExtracted] =>
@@ -53,12 +56,53 @@ trait OpenIELinked extends LinkedDocument {
     }
   }
 
-  private def cleanArg(sent: Sentence with Chunked)(arg: ExtractionPart): String = {
-    var tokens = arg.tokenIndices.map(index => sent.tokens(index))
+  private def cleanArg(sent: DocumentSentence[Sentence with OpenIEExtracted])(arg: ExtractionPart): Seq[(Char, Int)] = {
+    var tokens = arg.tokenIndices.map(index => sent.sentence.tokens(index))
     // drop determiners and prepositions from start/end
     if (tokens.headOption.exists(t => t.isDeterminer || t.isPreposition)) tokens = tokens.drop(1)
     if (tokens.lastOption.exists(t => t.isDeterminer || t.isPreposition)) tokens = tokens.dropRight(1)
-    tokens.map(_.string).mkString(" ")
+    tokens.flatMap { t =>
+      val baseOffset = t.offset + sent.offset
+      t.string.zipWithIndex.map { case (ch, index) => (ch, index + baseOffset) } :+ (' ', baseOffset + t.string.length)
+    }.dropRight(1)
+  }
+
+  /**
+   * iterate over subs and remove any Substitutions, by their interval, that overlap with earlier subs.
+   * e.g. ([0,1], [1,2], [2,3]) -> ([0,1], [2,3])
+   */
+  private def getNonOverlappingSubstitutions(subs: Seq[Substitution]): Seq[Substitution] = {
+    var subsToKeep = List.empty[Substitution]
+    var usedIntervals = List.empty[Interval]
+    for (s <- subs) {
+      val sInterval = Interval.open(s.mention.offset, s.mention.offset + s.mention.text.length)
+      if (!usedIntervals.exists(_.intersects(sInterval))) {
+        usedIntervals ::= sInterval
+        subsToKeep ::= s
+      }
+    }
+    subsToKeep
+  }
+
+
+  private def resolveArg(indexedText: Seq[(Char, Int)], ds: DocumentSentence[Sentence with OpenIEExtracted]) = {
+    val bestMentions = if (this.isInstanceOf[BestEntityMentionResolvedDocument]) {
+      (indexedText.headOption, indexedText.lastOption) match {
+        case (Some((_, chStart)), Some((_, chEnd))) =>
+          this.asInstanceOf[BestEntityMentionResolvedDocument].bestEntityMentionsBetween(indexedText.head._2, indexedText.last._2 + 1)
+        case _ => Nil
+      }
+
+    } else Nil
+    val subs = bestMentions.map(_.substitution)
+    val filtered = getNonOverlappingSubstitutions(subs)
+    if (filtered.isEmpty) indexedText.map(_._1).mkString
+    else {
+      val fixedSubs = filtered.map(_.fixPossessive)
+      val subIntervals = fixedSubs.map { s => (Interval.open(s.mention.offset, s.mention.offset + s.mention.text.length), s.best.text) }
+      val substituted = CoreferenceResolver.substitute(indexedText, subIntervals)
+      substituted.map(_._1).mkString
+    }
   }
 
   /**
@@ -67,10 +111,11 @@ trait OpenIELinked extends LinkedDocument {
   lazy val argContexts = this.sentences.flatMap { s =>
     // Get arguments to send to the linker
     val args = s.sentence.extractions.flatMap(e => e.arg1 :: e.arg2 :: Nil).distinct
-    val cleanArgs = args map cleanArg(s.sentence)
-
+    val cleanArgs = args map cleanArg(s)
+    val resolvedArgs = cleanArgs.map(ca => resolveArg(ca, s))
+    val foo = 1
     // Get context and cleaned-up form for each arg
-    args.zip(cleanArgs).map { case (arg, cleaned) =>
+    args.zip(resolvedArgs).map { case (arg, cleaned) =>
       val (extended, clusters) = if (this.isInstanceOf[CorefResolved]) {
         val argStartToken = s.sentence.tokens(arg.tokenIndices.head)
         val argEndToken = s.sentence.tokens(arg.tokenIndices.last)
